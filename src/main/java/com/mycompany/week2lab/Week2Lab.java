9eaca10e261d8ac36ea69d10154cf921196f1e90
jifang <jifang1218@gmail.com>
2023-01-12 16:13:34 -0700

    Assignment for Week2Lab.

---
+++ src/main/java/com/mycompany/week2lab/Week2Lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.week2lab;
+
+/**
+ *
+ * @author jifang
+ */
+public class Week2Lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
